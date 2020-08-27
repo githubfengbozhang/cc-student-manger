5d252c4cea18f865abde5a13cedcfa750f32cc3f
fengbaizhang <fengbaizhang@git.tb.com>
2020-08-27 23:31:38 +0800

    提交

--- src/views/PhoneExam/index.js
+++ src/views/PhoneExam/index.js
@@ -1,53 +1,75 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 // import { WingBlank, Picker, List, WhiteSpace } from 'antd-mobile';
 // import { createForm } from 'rc-form';
-import { PullToRefresh, ListView, Button } from 'antd-mobile';
+import { PullToRefresh, Button } from 'antd-mobile';
 import './index.scss'
-const data = [
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
-    title: 'Meet hotel',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
-    title: 'McDonald\'s invites you',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
-    title: 'Eat the week',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-];
 
-const NUM_ROWS = 20;
-let pageIndex = 0;
-
-function genData (pIndex = 0) {
-  const dataArr = [];
-  for (let i = 0; i < NUM_ROWS; i++) {
-    dataArr.push(`row - ${(pIndex * NUM_ROWS) + i}`);
-  }
-  return dataArr;
-}
 
 class Index extends Component {
   constructor(props) {
     super(props);
-    const dataSource = new ListView.DataSource({
-      rowHasChanged: (row1, row2) => row1 !== row2,
-    });
-
     this.state = {
-      dataSource,
-      refreshing: true,
-      isLoading: true,
+      refreshing: false,
+      down: true,
       height: document.documentElement.clientHeight,
-      useBodyScroll: false,
+      data: [],
     };
   }
-
+  genData () {
+    const dataArr = [];
+    for (let i = 0; i < 20; i++) {
+      dataArr.push(i);
+    }
+    return dataArr;
+  }
+  componentDidUpdate () {
+    if (this.state.useBodyScroll) {
+      document.body.style.overflow = 'auto';
+    } else {
+      document.body.style.overflow = 'hidden';
+    }
+  }
+  componentDidMount () {
+    const hei = this.state.height - ReactDOM.findDOMNode(this.ptr).offsetTop;
+    setTimeout(() => this.setState({
+      height: hei,
+      data: this.genData(),
+    }), 0);
+  }
+  render () {
+    return (<div>
+      <Button
+        style={{ marginBottom: 15 }}
+        onClick={() => this.setState({ down: !this.state.down })}
+      >
+        direction: {this.state.down ? 'down' : 'up'}
+      </Button>
+      <PullToRefresh
+        damping={60}
+        ref={el => this.ptr = el}
+        style={{
+          height: this.state.height,
+          overflow: 'auto',
+        }}
+        indicator={this.state.down ? {} : { deactivate: '上拉可以刷新' }}
+        direction={this.state.down ? 'down' : 'up'}
+        refreshing={this.state.refreshing}
+        onRefresh={() => {
+          this.setState({ refreshing: true });
+          setTimeout(() => {
+            this.setState({ refreshing: false });
+          }, 1000);
+        }}
+      >
+        {this.state.data.map(i => (
+          <div key={i} style={{ textAlign: 'center', padding: 20 }}>
+            {this.state.down ? 'pull down' : 'pull up'} {i}
+          </div>
+        ))}
+      </PullToRefresh>
+    </div>);
+  }
 }
 // const IndexFrom = createForm()(Index);
 export default Index;
